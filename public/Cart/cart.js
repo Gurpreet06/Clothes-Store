ecde880e0ba320e52e6578152526b7bcb32da997
Gurpreet Singh <74554439+Gurpreet06@users.noreply.github.com>
2021-06-06 15:40:33 +0200

    'CartModify'

--- public/Cart/cart.js
+++ public/Cart/cart.js
@@ -23,15 +23,23 @@ function addItemCart(Name, Price) {
     <div class="element" id='cartOne'>
     <div class="eleinfo">
                     <h2 class="eleh2">${Name}</h2>
-                    <p class="elep">${Price}</p>
+                    <div class='origniaLPrice'>
+                    <p>Original Price:</p>
+                    <p class="elep" id='oriPrice'> ${Price}</p>
+                    </div>
+
+                    <div class='origniaLPrice'>
+                    <p>Total Price:</p>
+                    <p class="elep fiexPrice" id='pdtPrice'>${Price}</p>
+                    </div>
                     <div class="item">
-                        <ion-icon name="remove-circle-outline" class="iocon"></ion-icon>
-                        <div class="countItem">1</div>
-                        <ion-icon name="add-circle-outline" class="iocon"></ion-icon>
+                        <ion-icon name="remove-circle-outline" class="iocon" id='restPrice' onclick='restProduct(1, this.nextElementSibling, this.parentElement.parentElement.firstElementChild.nextElementSibling.lastElementChild, this.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling.lastElementChild)'></ion-icon>
+                        <div class="countItem" id='countItem'>1</div>
+                        <ion-icon name="add-circle-outline" class="iocon" id='addPrice' onclick='addProduct(1, this.parentElement.firstElementChild.nextElementSibling, this.parentElement.parentElement.firstElementChild.nextElementSibling.lastElementChild, this.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling.lastElementChild)'></ion-icon>
                     </div>
                     </div>
                     <div>
-                    <ion-icon name="close-outline" onclick='delteEvents()' style='cursor: pointer;'></ion-icon>
+                    <ion-icon name="close-outline" onclick='delteEvents(this.parentElement.parentElement)' id='delll' style='cursor: pointer;'></ion-icon>
 </div>
 </div>`
 
@@ -39,10 +47,9 @@ function addItemCart(Name, Price) {
 
 
 // Delte events
-function delteEvents() {
-    let o = document.getElementById('cartOne')
-    o.parentElement.removeChild(o)
-
+function delteEvents(parntEle) {
+    let o = parntEle
+    o.remove()
 
     let Name = localStorage.getItem('Name')
     let Price = localStorage.getItem('Price')
@@ -59,6 +66,43 @@ function delteEvents() {
 
     localStorage.setItem('Name', JSON.stringify(title))
     localStorage.setItem('Price', JSON.stringify(price))
-
     location.reload()
+}
+
+async function addProduct(num, id, origPrice, totalPrice) {
+    let addPrice = id
+    addPrice.innerHTML = parseFloat(addPrice.innerHTML) + num
+    let orignalPrice = origPrice.innerText
+    let y = parseFloat(orignalPrice) * addPrice.innerHTML
+
+    totalPrice.innerText = parseFloat(y).toFixed(2) + '€'
+
+    let subTotl = document.getElementById('subTotl')
+    let totlSb = document.getElementById('lasttd')
+    let fiexPrice = document.querySelectorAll('.fiexPrice')
+    fiexPrice.forEach(element => {
+        subTotl.innerHTML = parseFloat(element.innerText).toFixed(2) + '€'
+        totlSb.innerHTML = parseFloat(element.innerText).toFixed(2) + '€'
+    });
+}
+
+async function restProduct(num, id, origPrice, totalPrice) {
+    let countItem = id
+    if (countItem.innerHTML == 1) {
+        countItem.innerHTML = 1
+    } else {
+        countItem.innerHTML = parseFloat(countItem.innerHTML) - num
+        let orignalPrice = origPrice.innerText
+        let y = parseFloat(totalPrice.innerText) - parseFloat(orignalPrice)
+
+        totalPrice.innerText = parseFloat(y).toFixed(2) + '€'
+    }
+
+    let subTotl = document.getElementById('subTotl')
+    let totlSb = document.getElementById('lasttd')
+    let fiexPrice = document.querySelectorAll('.fiexPrice')
+    fiexPrice.forEach(element => {
+        subTotl.innerHTML = parseFloat(element.innerText).toFixed(2) + '€'
+        totlSb.innerHTML = parseFloat(element.innerText).toFixed(2) + '€'
+    });
 }
